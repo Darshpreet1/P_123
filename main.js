794ebdf60e854f3317051b1792d60409eba1e418
Darshpreet1 <102862814+Darshpreet1@users.noreply.github.com>
2022-10-26 20:08:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+    function preload(){
+
+    }
+    
+    function setup(){
+        Canvas = createCanvas(300,300);
+        Canvas.center();
+    }
+    
+    function draw(){
+    
+    }
+    
+    function take_snapshot(){
+        save("filtered_image.jpg");
+    }
